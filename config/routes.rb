13cb384046aa1ac7e0016ba659d46cba6d772d2e
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-09-22 12:51:23 -0400

    added bookshelf route

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: %i[create]
       post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
+      get '/bookshelf', to: 'users#bookshelf'
+      get '/search', to: 'users#search'
+      post '/book_search', to: 'books#book_search'
       resources :bookshelves, only: [:index, :create]
       resources :bookshelf_books, only: [:index, :create]
       resources :books, only: [:index, :create]
-      post '/book_search', to: 'books#book_search'
     end
   end
 end
